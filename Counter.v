8d1b76102fc0f8bef0ef9c36d9eee82c70d42bca
mayaank2512 <mayaank2512@gmail.com>
2025-09-16 15:17:23 +0530

    Create Counter.v

---
+++ Counter.v
@@ -0,0 +1,8 @@
+module Counter(input clk, input reset, output reg [3:0] count);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 4'b0000;
+        else
+            count <= count + 1;
+    end
+endmodule
